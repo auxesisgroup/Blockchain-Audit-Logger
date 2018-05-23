74b8963a803ffb8ffb99e8b73e4520f68463bca3
Sweta Mishra <sweta@auxesisgroup.com>
2018-05-23 19:27:51 +0530

    data

---
+++ src/test/java/com/auxesis/auditlogger/CashaaAuditLoggerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.auxesis.auditlogger;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CashaaAuditLoggerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
